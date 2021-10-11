6f4d210f0644161fdb012d04b2a6c3e12c8c7cf8
Zedong Peng <36201209+pengzedong@users.noreply.github.com>
2021-10-11 12:47:17 -0400

    Add files via upload

---
+++ MT-checker-python.py
@@ -0,0 +1,33 @@
+import sys
+
+
+def check(a, b, c):
+    
+    #print('the a is', a)
+    #print('the b is', b)
+    if (int(c)<int(a)+int(b)):
+      # if (int(c)<abs(int(a))-abs(int(b))):
+      #     t=int(c)<abs(int(a))-abs(int(b))
+      #print(t)
+      t=abs(int(a))-abs(int(b))
+      if (abs(t)<int(c)):
+        #print(abs(t))
+        print("pass")
+        return True
+      else:
+        print("invalid")
+        return False
+    else:
+        print("invalid")
+        return False
+
+
+    
+    
+if __name__ == '__main__':
+    try:
+        a, b, c= sys.argv[1:5]
+        check(a, b, c)
+    except Exception as e:
+        print(sys.argv)
+        print(e)
